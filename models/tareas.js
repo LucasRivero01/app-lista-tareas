0e67df07f01acdfe338fed8e37414268cafd45c9
LucasRivero01 <lucas.rivero@naranjax.com>
2022-02-02 18:08:09 -0300

    tarea6

--- models/tareas.js
+++ models/tareas.js
@@ -1,9 +1,16 @@
+const Tarea = require("./tarea");
+
 class Tareas{
    _listado = {}
 
    constructor(){
       //se inicializa el listado
       this._listado = {};
+   }
+
+   crearTarea(desc){
+      const tarea = new Tarea(desc);
+      this._listado[tarea.id] = tarea;
    }
 }
 
